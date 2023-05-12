7bf50e9644de7b0204beb46e2aa4472efd38a1ea
marouane <marwan.bmw11@gmail.com>
2023-05-13 00:00:03 +0100

    msg

---
+++ hh.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main (){
+
+int salma = 44;
+
+ if  (salma == 44){
+
+printf("yes salma = 44 ");
+
+} else if  (salma == 54){ printf(" yes salma = 45 ");
+
+
+}else if  (salma == 77){ printf("yes salma = 77");
+
+}
+else
+{
+    printf("this number not found");
+}
+
+
+
+}
