b46da2aabe64707b966af3c3684b613bae0fed44
marouane <marwan.bmw11@gmail.com>
2023-05-13 00:07:27 +0100

    msg

--- hh.c
+++ hh.c
@@ -2,16 +2,16 @@
 
 int main (){
 
-int salma = 44;
+int wall = 44;
 
- if  (salma == 44){
+ if  (wall == 44){
 
-printf("yes salma = 44 ");
+printf("yes wall = 44 ");
 
-} else if  (salma == 54){ printf(" yes salma = 45 ");
+} else if  (wall == 54){ printf(" yes wall = 45 ");
 
 
-}else if  (salma == 77){ printf("yes salma = 77");
+}else if  (wall == 77){ printf("yes wall = 77");
 
 }
 else
